a6a375f08a09a94a0aae91057340782102eef2d0
IvoBraatz <braatzivo@hotmail.com>
2024-04-23 17:08:48 -0300

    dinamic

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-document.getElementById('generate').addEventListener('click', function() {
+// Adicione um evento de escuta nos elementos relevantes
+document.getElementById('length').addEventListener('input', generatePassword);
+document.getElementById('uppercase').addEventListener('change', generatePassword);
+document.getElementById('lowercase').addEventListener('change', generatePassword);
+document.getElementById('numbers').addEventListener('change', generatePassword);
+document.getElementById('special').addEventListener('change', generatePassword);
+
+// Função para gerar a senha
+function generatePassword() {
     var length = parseInt(document.getElementById('length').value);
     var uppercase = document.getElementById('uppercase').checked;
     var lowercase = document.getElementById('lowercase').checked;
     var numbers = document.getElementById('numbers').checked;
     var special = document.getElementById('special').checked;
-  
+
     var charset = '';
     if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
     if (numbers) charset += '0123456789';
     if (special) charset += '!@#$%^&*()_+{}[]|;:,.<>?';
-  
+
     var password = '';
     for (var i = 0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset[randomIndex];
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
     }
-  
+
     document.getElementById('password').value = password;
-  });
-  
+}
 
-  const range = document.getElementById('length');
-  const rangeValue = document.getElementById('rangeValue');
-
-  range.addEventListener('input', () => {
-    rangeValue.textContent = range.value;
-  });
+// Chame a função inicialmente para gerar a senha ao carregar a página
+generatePassword();
